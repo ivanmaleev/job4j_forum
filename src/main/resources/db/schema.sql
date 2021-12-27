578854dd814523ef6edd99d32b5210b6d97e7096
ivanmaleev@inbox.ru <mudr6PHEL9$>
2021-12-27 19:23:37 +0300

    add Procfile

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,16 +1,16 @@
-create table posts (
+create table if not exists posts (
   id serial primary key,
   name varchar(2000),
   description text,
   created timestamp without time zone not null default now()
 );
 
-CREATE TABLE authorities (
+CREATE TABLE if not exists authorities (
   id serial primary key,
   authority VARCHAR(50) NOT NULL unique
 );
 
-CREATE TABLE users (
+CREATE TABLE if not exists users (
   id serial primary key,
   username VARCHAR(50) NOT NULL unique,
   password VARCHAR(100) NOT NULL,
